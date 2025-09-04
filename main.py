e0e79d818b3ad8b26b00e506d44c1a74ebc24a2d
davidszaboo <szabodavid5@outlook.com>
2025-09-04 18:41:37 +0200

    Második módosítás - Neptun kód beillesztve.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('üdv a Szkript nyelvek órán! Első módosítás.')
+    print_hi('üdv a Szkript nyelvek órán! Első módosítás. - AIYKZM')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
