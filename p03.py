2c9cae2d146620c8eadb1e8ddabe590ca2e2473e
davidszaboo <szabodavid5@outlook.com>
2025-09-18 10:22:53 +0200

    3. alkalom - új modul

---
+++ p03.py
@@ -0,0 +1,48 @@
+#3. alkalom
+def szam_bekerese(legnagyobb_szam):
+    szam = input("Kérek egy számot: ")
+    if szam.isdigit():
+        szam = int(szam)
+        if szam == 0:
+            print("Nem ismerem a 0 számot")
+        elif szam > legnagyobb_szam:
+            print("Túl nagy számot adtál meg")
+        else:
+            print("Most már tudok a számmal dolgozni")
+    else:
+        print("Nem számot írtál")
+    return szam
+
+def szamologep():
+    muvelet = input("Milyen műveletet akarsz végrehajtani? (+,-,*,/): ")
+    egyik_szam = szam_bekerese(10)
+    masik_szam = szam_bekerese(100)
+    if muvelet == "+":
+        eredmeny = egyik_szam + masik_szam
+    elif muvelet == "-":
+        eredmeny = egyik_szam - masik_szam
+    elif muvelet == "*":
+        eredmeny = egyik_szam * masik_szam
+    else:
+        eredmeny = egyik_szam / masik_szam
+    print(f"Az eredmény: {egyik_szam} {muvelet} {masik_szam} = {eredmeny}")
+
+def veletlenszam(max):
+    import random
+    szam = random.randint(0, max)
+    return szam
+
+def egesz_szam_bekerese():
+    while True:
+        szam = input("Kérek egy egész számot: ")
+        try:
+            szam = int(szam)
+            break
+        except ValueError:
+            print("Nem egész számot adtál meg")
+    return szam
+
+#Program
+if __name__ == "__main__":
+    print(egesz_szam_bekerese())
+
